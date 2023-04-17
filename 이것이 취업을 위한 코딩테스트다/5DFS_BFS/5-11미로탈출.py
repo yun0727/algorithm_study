d4a7e0fcbe8c6f200f4aac6bbb5e43f9e8881f6a
yun <austru00@gmail.com>
2023-04-17 17:35:38 +0900

    이취코 05 DFS/BFS

--- 이것이 취업을 위한 코딩테스트다/5DFS_BFS/5-11미로탈출.py
+++ 이것이 취업을 위한 코딩테스트다/5DFS_BFS/5-11미로탈출.py
@@ -6,7 +6,6 @@ n,m=map(int, input().split())
 graph=[]
 for i in range(n):
     graph.append(list(map(int,input())))
-    
 #이동할 네 방향 정의
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
@@ -17,14 +16,14 @@ def bfs(x,y):
     queue=deque()
     queue.append((x,y))
     # 큐가 빌 때까지 반복
-    while deque:
-        x,y=deque.popleft()
+    while queue:
+        x,y=queue.popleft()
         #현재 위치에서 네 방향으로의 위치 확인
         for i in range(4):
             nx=x+dx[i]
             ny=y+dy[i]
             # 미로 찾기 공간을 벗어난 경우 무시
-            if nx < 0 or ny<0 or nx>=n or ny>=m:
+            if nx<0 or ny<0 or nx>=n or ny>=m:
                 continue
             #벽인 경우 무시
             if graph[nx][ny] == 0:
