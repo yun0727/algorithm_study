fc5251a556c49039fbf4108f4a1f3f4311231703
yun <austru00@gmail.com>
2023-04-17 16:58:07 +0900

    DFS/BFS

--- 이것이 취업을 위한 코딩테스트다/6정렬.py
+++ 이것이 취업을 위한 코딩테스트다/6정렬.py
@@ -28,3 +28,26 @@
 # a_result=a[:n-k]
 # b_result=b[:k]
 # print(sum(a_result)+sum(b_result))
+
+# #10825 국영수
+# n=int(input())
+# students=[]
+# for i in range(n):
+#     students.append(input().split())
+# students.sort(key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]),x[0]))
+# for student in students:
+#     print(student[0])
+    
+# #18310 
+# n=int(input())
+# num=list(map(int,input().split()))
+# num.sort()
+# print(num[(n-1)//2])
+
+# #1715
+# n=int(input())
+# num=[]
+# for i in range(n):
+#     num.append(input())
+    
+# print(num)
